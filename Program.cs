2aaffa29317df1deca91396ff87eda0ea54380b8
Pybounce <pybounce@protonmail.com>
2025-11-05 15:25:07 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Net;
+
+class Lox
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 1) { throw new Exception("Why in the seven hells are you adding multiple arguments ARE YOU CRAZY"); }
+        else if (args.Length == 1) {
+            RunFile(args[0]);
+        }
+        else {
+            RunPrompt();
+        }
+    }
+
+
+
+    static void RunFile(String path)
+    {
+
+    }
+
+    static void RunPrompt()
+    {
+        
+    }
+}
+
