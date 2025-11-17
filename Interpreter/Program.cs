9b6286d76de1c1890b9529f51a82ed104952461e
Pybounce <pybounce@protonmail.com>
2025-11-17 08:13:22 +0000

    built in examples and help

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -9,13 +9,74 @@ class Lox
 
     static void Main(string[] args)
     {
+        if (TryListExamples(args)) { return; }
+        if (TryShowHelp(args)) { return; }
+        if (TryRunExample(args)) { return; }
 
         if (args.Length == 1) { RunFile(args[0]); }
         else if (args.Length == 0) { RunPrompt(); }
         else
         {
-            throw new ArgumentException("Must have less than 2 arguments");
+            throw new ArgumentException("Use 'dotnet run --help' for instructions.");
         }
+    }
+
+    static bool TryShowHelp(string[] args)
+    {
+        if (args == null || args.Length != 1 || args[0] != "--help") { return false; }
+
+        Console.WriteLine("");
+        Console.WriteLine("------------ Instructions ------------");
+        Console.WriteLine("Use 'dotnet run' to run lox in REPL mode.");
+        Console.WriteLine("Use 'dotnet run FILE_PATH' to run a lox script found at FILE_PATH.");
+        Console.WriteLine("Use 'dotnet run --examples EXAMPLE_NAME' to run a built in example.");
+        Console.WriteLine("Use 'dotnet run --examples' to list all available example names.");
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine("");
+
+        return true;
+    }
+
+    static bool TryListExamples(string[] args)
+    {
+        if (args == null || args.Length != 1 || args[0] != "--examples") { return false; }
+        var examplesDir = GetExamplesDirectory();
+        Console.WriteLine("");
+        Console.WriteLine("------------ Examples ------------");
+        foreach (var example_path in System.IO.Directory.EnumerateFiles(examplesDir))
+        {
+            Console.WriteLine(Path.GetFileNameWithoutExtension(example_path));
+        }
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine("");
+        return true;
+    }
+
+    static bool TryRunExample(string[] args)
+    {
+        if (args == null || args.Length != 2 || args[0] != "--examples") { return false; }
+
+        try
+        {
+            var example_name = args[1];
+            var path = Path.Combine(GetExamplesDirectory(), $"{example_name.ToLower()}.dat");
+            RunFile(path);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Failed to run example. {e.Message}");
+            return true;
+        }
+        return true;
+
+    }
+
+    static string GetExamplesDirectory()
+    {
+        var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        var projectDir = Directory.GetParent(baseDir)?.Parent?.Parent?.Parent?.FullName;
+        var path = Path.Combine(projectDir!, "examples");
+        return path;
     }
 
     static void RunPrompt()
